1e9e835ee2f214284f84a8d4a9faec73beae6727
mirko93 <skopljak.mirko93@gmail.com>
2020-05-06 14:34:43 +0200

    refining the API (tokens, refactoring to policy, http responses...)

--- app/Contact.php
+++ app/Contact.php
@@ -11,8 +11,18 @@ class Contact extends Model
 
     protected $dates = ['birthday'];
 
+    public function path()
+    {
+        return url('/contacts/' . $this->id);
+    }
+
     public function setBirthdayAttribute($birthday)
     {
         $this->attributes['birthday'] = Carbon::parse($birthday);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->group(function () {
 
+    Route::get('/contacts', 'ContactsController@index');
     Route::post('/contacts', 'ContactsController@store');
     Route::get('/contacts/{contact}', 'ContactsController@show');
     Route::patch('/contacts/{contact}', 'ContactsController@update');
     Route::delete('/contacts/{contact}', 'ContactsController@destroy');
-    
+
 });
 
