a2aafae00c68572988cd3ce6bae630d1ac953dcf
mirko93 <skopljak.mirko93@gmail.com>
2020-05-06 20:29:47 +0200

    MySQL setup and User Registration

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+Route::get('/logout-manual', function () {
+    request()->session()->invalidate();
+});
+
 Route::get('/{any}', 'AppController@index')->where('any', '.*');
